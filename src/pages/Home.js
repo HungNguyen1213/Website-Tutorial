9634776d4e024d3a7ea5a9502addcf38ba2ad8e0
HungNguyen1213 <52905968+HungNguyen1213@users.noreply.github.com>
2022-01-18 23:43:46 +0700

    upload

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Cards, HeroSection, Footer } from "../components";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
